30bcfd83c0be56544b1b104e572d43ba27d0a762
michaellindsay23 <michaelduffielindsay@gmail.com>
2025-10-27 13:42:53 -0400

    implementing lsps

--- plugins/lsp.nix
+++ plugins/lsp.nix
@@ -1,10 +1,24 @@
 { ... }:
 {
-  lsp.servers = {
-    nixd.enable = true;
-    rust-analyzer.enable = true;
-    java-lanuage-server.enable = true;
-    lua-ls.enable = true;
-    clangd.enable = true;
+  plugins.lsp = {
+    enable = true;
+
+    servers = {
+      clangd.enable = true;
+
+      #java-lanuage-server.enable = true;
+
+      lua_ls.enable = true;
+
+      nixd.enable = true;
+
+      pyright.enable = true;
+
+      rust_analyzer = {
+        enable = true;
+	installCargo = false;
+	installRustc = false;
+      };
+    };
   };
 }
