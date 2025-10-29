975fafe84eb67d9c95f26b031fe09d1fd6fb0d33
michaellindsay23 <michaelduffielindsay@gmail.com>
2025-10-29 17:28:32 -0400

    fix neotree settings

--- plugins/neo-tree.nix
+++ plugins/neo-tree.nix
@@ -2,9 +2,11 @@
 {
   plugins.neo-tree = {
     enable = true;
+    
+    settings = {
+      auto_clean_after_session_restore = true;
 
-    auto_clean_after_session_restore = true;
-
-    close_if_last_window = true;
+      close_if_last_window = true;
+    };
   };
 }
