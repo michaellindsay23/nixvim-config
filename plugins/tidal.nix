2d62dddc83615dcda307d2f9800470a99a7b0e49
michaellindsay23 <michaelduffielindsay@gmail.com>
2025-12-11 13:20:24 -0500

    update

--- plugins/tidal.nix
+++ plugins/tidal.nix
@@ -1,16 +1,7 @@
 { inputs, pkgs, ... }:
 {
-  extraConfigLua = ''
-    require("grddavies/tidal.nvim")'';
-#	  opts = {
-#	    -- Your configuration here
-#	    -- See configuration section for defaults
-#	  },
-#	  -- Recommended: Install TreeSitter parsers for Haskell and SuperCollider
-#	  dependencies = {
-#		  "nvim-treesitter/nvim-treesitter",
-#		  opts = { ensure_installed = { "haskell", "supercollider" } },
-#	  },
-#  '';
+  plugins = [
+   vim-tidal
+  ];
 }
 
