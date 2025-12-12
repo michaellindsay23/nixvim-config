4805f96f084731e2b6951f6b4dae10eefab89510
michaellindsay23 <michaelduffielindsay@gmail.com>
2025-12-11 19:12:23 -0500

    packaging tidal.nvim

--- plugins/default.nix
+++ plugins/default.nix
@@ -5,12 +5,13 @@
     ./cmp.nix
     ./cord.nix
     ./cursorline.nix
+    ./lazy.nix
     ./lsp.nix
     ./lualine.nix
     ./neo-tree.nix
     ./precognition.nix
     ./telescope.nix
-    ./tidalnvim.nix
+    #./tidalnvim.nix
     ./treesitter.nix
     ./trouble.nix
     ./which-key.nix
